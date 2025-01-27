d2edc4b8c4a6439a9c1c0e953c7196498c6d3c81
pengjianhong5 <pengjianhong5@jd.com>
2025-01-27 11:55:40 +0800

    config oss storage

--- packages/cms/config/middlewares.ts
+++ packages/cms/config/middlewares.ts
@@ -1,12 +1,29 @@
-export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
-];
+export default function ({env}: {env: any}) {
+  return [
+    'strapi::logger',
+    'strapi::errors',
+    'strapi::cors',
+    'strapi::poweredBy',
+    'strapi::query',
+    'strapi::body',
+    'strapi::session',
+    'strapi::favicon',
+    'strapi::public',
+    {
+      name: 'strapi::security',
+      config: {
+        contentSecurityPolicy: {
+          useDefaults: true,
+          directives: {
+            'img-src': [
+              "'self'",
+              'data:',
+              'blob:',
+              '*.oss-cn-beijing.aliyuncs.com',
+            ],
+          },
+        },
+      },
+    }
+  ];
+}
--- packages/cms/config/plugins.ts
+++ packages/cms/config/plugins.ts
@@ -1,1 +1,13 @@
-export default () => ({});
+export default ({env}: {env: any}) => ({
+    upload: {
+        config: {
+          provider: 'strapi-provider-upload-oss', // full package name is required
+          providerOptions: {
+            accessKeyId: env('OSS_ACCESS_KEY_ID'),  // required
+            accessKeySecret: env('OSS_ACCESS_KEY_SECRET'), // required
+            region: env('OSS_REGION'), // required
+            bucket: env('OSS_BUCKET'), // required
+          }
+        }
+      }
+});
